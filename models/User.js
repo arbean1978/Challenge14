3380ba4163f1f3f419eead750d4c91a179d76a47
Aaron Bean <arbean1978@gmail.com>
2022-03-25 09:30:02 -0600

    updateuserseedsandusermodel

--- models/User.js
+++ models/User.js
@@ -2,48 +2,70 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-
+// create the User model
 class User extends Model {
+
     checkPassword(loginPw) {
         return bcrypt.compareSync(loginPw, this.password);
     }
 }
 
-User.init({
+User.init(
+  {
+
     id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true
-    },
+        },
+    
     username: {
         type: DataTypes.STRING,
-        allowNull: false
-    },
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        }
+        },
+    
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+            }
+        },
+    
     password: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
+            // this means the password must be at least four characters long
             len: [4]
+            }
         }
-    }
-}, {
+  },
+  {
+    //
     hooks: {
         async beforeCreate(newUserData) {
             newUserData.password = await bcrypt.hash(newUserData.password, 10);
             return newUserData;
-        },
+          },
+        
         async beforeUpdate(updatedUserData) {
             updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
             return updatedUserData;
-        }
+          }
     },
+    
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'user'
-})
-
+  }
+);
 
 module.exports = User;
